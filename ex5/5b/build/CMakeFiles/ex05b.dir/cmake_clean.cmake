ada630c8dbb672318c7a1e9668d6ee963bd3f567
davento <root@DESKTOP-OCM2FOF.localdomain>
2020-04-14 23:26:20 -0500

    added build

---
+++ ex5/5b/build/CMakeFiles/ex05b.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex05b.dir/src/main.cpp.o"
+  "CMakeFiles/ex05b.dir/src/number.cpp.o"
+  "ex05b.pdb"
+  "ex05b"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex05b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
